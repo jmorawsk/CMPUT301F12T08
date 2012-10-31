08cf6fc5ad98b069f6b5d1cc1dccc1a71cf0245d
Jason Morawski <jmorawsk@ualberta.ca>
2012-10-31 10:10:16 -0600

    Added db stuff

---
+++ TaskTracker/src/dbmanager/tempdbtest.java
@@ -0,0 +1,25 @@
+package dbmanager;
+
+import junit.framework.TestCase;
+
+
+public class tempdbtest extends TestCase
+{
+    //    @Test
+    //    public void testReset()
+    //    {
+    //        dbmanager.nukeAll();
+    //        String emptyList = dbmanager.listTasks();
+    //        assertTrue(emptyList.equals("[]"));
+    //    }
+    //    @Test
+    //    public void testAdd()
+    //    {
+    //        dbmanager.nukeAll();
+    //        dbmanager.insertTask("test Summary", "test description");
+    //        String myList = dbmanager.listTasks();
+    //        System.out.println(myList);
+    //        System.out.println(dbmanager.listTasks());
+    //        assertFalse(myList.equals("[]"));
+    //    }
+}
