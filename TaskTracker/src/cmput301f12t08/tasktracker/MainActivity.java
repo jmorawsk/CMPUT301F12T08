356188c5d9ad7366589981f2f9f8c4f96c4a4870
jbonot <jbonot@ualberta.ca>
2012-10-31 18:42:17 -0600

    Removed CrowdSourcer project, added TaskTracker project

---
+++ TaskTracker/src/cmput301f12t08/tasktracker/MainActivity.java
@@ -0,0 +1,21 @@
+package cmput301f12t08.tasktracker;
+
+import cmput301f12t08.tasktracker.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
