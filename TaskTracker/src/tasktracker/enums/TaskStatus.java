76ff22da9e044a091e667b874d79e983fb95ce15
jbonot <jbonot@ualberta.ca>
2012-10-31 19:50:32 -0600

    Created

---
+++ TaskTracker/src/tasktracker/enums/TaskStatus.java
@@ -0,0 +1,10 @@
+package tasktracker.enums;
+
+/**
+ * An enum to indicate the status of a task.  Can be expanded in the future.
+ * @author jbonot
+ *
+ */
+public enum TaskStatus {
+	Fulfilled, Unfulfilled
+}
