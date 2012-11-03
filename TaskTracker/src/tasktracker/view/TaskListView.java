49d9de87c5d03c3346b04747ef9aca2a1838e127
jbonot <jbonot@ualberta.ca>
2012-11-03 12:36:33 -0600

    Added private properties to TaskListView.java

--- TaskTracker/src/tasktracker/view/TaskListView.java
+++ TaskTracker/src/tasktracker/view/TaskListView.java
@@ -3,18 +3,34 @@ package tasktracker.view;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.*;
+import tasktracker.elements.*;
+import java.util.*;
 
+/**
+ * An activity that displays the user's tasks. The user is able to see the
+ * details of each task, add a new task, or delete a task.
+ * 
+ * @author Jeanine Bonot
+ * 
+ */
 public class TaskListView extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private ListView taskList;
+	private List<TaskElement> tasks;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
 }
