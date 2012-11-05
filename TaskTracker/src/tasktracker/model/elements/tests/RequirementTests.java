dfbe8d50f40bd436c19b4d46e2943de74c9a5bef
jbonot <jbonot@ualberta.ca>
2012-11-04 22:38:08 -0700

    Added RequirementTests (unimplemented)

---
+++ TaskTracker/src/tasktracker/model/elements/tests/RequirementTests.java
@@ -0,0 +1,45 @@
+package tasktracker.model.elements.tests;
+
+/**
+ * TaskTracker
+ * 
+ * Copyright 2012 Jeanine Bonot, Michael Dardis, Katherine Jasniewski,
+ * Jason Morawski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may 
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+ */
+
+import org.junit.*;
+
+/**
+ * Tests the Requirement class and subclasses.
+ * @author Jeanine Bonot
+ *
+ */
+public class RequirementTests {
+
+	/** Tests the fulfill method of the PhotoRequirement subclass */
+	@Test
+	public void testPhotoFulfill(){
+		// TODO: Photo taken
+		// TODO: Cancelled
+	}
+	
+	/** Tests the fulfill method of the TextRequirement subclass */
+	@Test
+	public void testTextFulfill(){
+		// TODO: Text too long
+		// TODO: Regular string
+		// TODO: Left empty
+		// TODO: Whitespace
+	}
+}
