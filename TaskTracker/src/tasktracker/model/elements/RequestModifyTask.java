eeab6473b399f9961d58776a302e7612c5a8f7bc
Jason Morawski <jmorawsk@ualberta.ca>
2012-12-01 14:42:02 -0700

    New method for updating web tasks

---
+++ TaskTracker/src/tasktracker/model/elements/RequestModifyTask.java
@@ -0,0 +1,87 @@
+package tasktracker.model.elements;
+
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import tasktracker.controller.DatabaseAdapter;
+import tasktracker.model.AccessURL;
+import tasktracker.model.NetworkRequestModel;
+import tasktracker.model.Preferences;
+import android.content.Context;
+import android.widget.Toast;
+
+
+public class RequestModifyTask implements NetworkRequestModel {
+    private Context context;
+    private Task task;
+    private String requestString;
+
+    static final Gson gson = new Gson();
+
+
+    public RequestModifyTask(Context contex, Task theTask) {
+        context = contex;
+        task = theTask;
+        String content = gson.toJson(task);
+        String command = "action=" + "update" + "&summary=" + task.getSummary()
+                + "&content=" + content.toString()
+                + "&id=" + task.getID();
+
+        requestString = AccessURL.turnCommandIntoURL(command);
+
+        AccessURL access = new AccessURL(this);
+        access.execute(getCrowdsourcerCommand());
+    }
+
+    public Context getContext()
+    {
+        return context;
+    }
+    public String getCrowdsourcerCommand()
+    {
+        // System.out.println("Request to network: " + requestString);
+        return requestString;
+    }
+    public void runAfterExecution(String line) {
+        /*
+        DatabaseAdapter _dbHelper = new DatabaseAdapter(context);
+
+        String taskID = AccessURL.getTag("id\":\"", line,
+                line.indexOf('}', 0) + 1);
+
+        // Save task to local SQL
+        List<String> others = task.getOtherMembers();
+
+        // Add to SQL server
+        _dbHelper.open();
+
+        task.setID(taskID);
+
+        //taskID = _dbHelper.createTask(task);
+        _dbHelper.createTask(task);
+
+        String taskName = task.getName();
+        String message = Notification.getMessage(
+                Preferences.getUsername(context), taskName,
+                Notification.Type.InformMembership);
+
+        //TODO: Waiting on refactor, taskID needs to be type String not long
+
+        _dbHelper.createMember(taskID,
+                Preferences.getUsername(context));
+
+        for (String member : others) {
+            _dbHelper.createMember(taskID, member);
+            _dbHelper.createNotification(taskID, member, message);
+        }
+
+        _dbHelper.close();
+        */
+
+        Toast toast = Toast.makeText(context,
+                "Updated crowdSourcer task: " + task.getName(),
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
