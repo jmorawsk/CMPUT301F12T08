25180aa59cdbdd4e1bfe3958adc4aed805ee0209
Jason Morawski <jmorawsk@ualberta.ca>
2012-12-04 21:59:55 -0700

    Licensed and javadoc'd taskmembers

--- TaskTracker/src/tasktracker/model/elements/TaskMembers.java
+++ TaskTracker/src/tasktracker/model/elements/TaskMembers.java
@@ -1,25 +1,55 @@
 package tasktracker.model.elements;
 
+/**
+ * TaskTracker
+ * 
+ * Copyright 2012 Jeanine Bonot, Michael Dardis, Katherine Jasniewski,
+ * Jason Morawski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may 
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+ */
 
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Class for handling the members of a task
+ * @author Jason
+ *
+ */
 public class TaskMembers
 {
 
     private List<String> _otherMembersList;
 
+    /**
+     * Gets the members from TaskMembers
+     * @return the list of members
+     */
     public List<String> get_otherMembersList()
     {
-
         return _otherMembersList;
     }
 
+    /**
+     * Sets the members for TaskMembers
+     * 
+     * @param _otherMembersList 
+     */
     public void set_otherMembersList(List<String> _otherMembersList)
     {
-
-        this._otherMembersList = _otherMembersList;
+        this._otherMembersList = new ArrayList<String>();
+        this._otherMembersList.addAll(_otherMembersList);
     }
 
     /**
