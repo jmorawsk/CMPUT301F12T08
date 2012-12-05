cad231e85f06da626ccf9583ec0bace75d483bc1
Jason Morawski <jmorawsk@ualberta.ca>
2012-12-04 21:39:07 -0700

    Add license, some javadoc to database adapter

--- TaskTracker/src/tasktracker/controller/DatabaseAdapter.java
+++ TaskTracker/src/tasktracker/controller/DatabaseAdapter.java
@@ -1,8 +1,25 @@
 package tasktracker.controller;
 
+/**
+ * TaskTracker
+ * 
+ * Copyright 2012 Jeanine Bonot, Michael Dardis, Katherine Jasniewski,
+ * Jason Morawski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may 
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+ */
+
 import java.util.ArrayList;
 import java.util.List;
-
 import tasktracker.model.DatabaseModel;
 import tasktracker.model.elements.Task;
 import android.content.ContentValues;
@@ -12,6 +29,10 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+/**
+ * Class for interacting with the local SQL database
+ *
+ */
 public class DatabaseAdapter {
 
 	public DatabaseModel databaseModel = new DatabaseModel();
